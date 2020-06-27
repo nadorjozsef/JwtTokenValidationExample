3c876db168dc66c9687d89a0198be24745b0ade1
nadorjozsef <nadorjozsef@gmail.com>
2020-06-27 22:22:07 +0200

    Add project files.

---
+++ JwtTokenValidationExample/Controllers/PageController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace JwtTokenValidationExample.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class PageController : ControllerBase
+    {
+        public PageController(ILogger<PageController> logger)
+        {
+            myLogger = logger;
+        }
+
+        [Authorize]
+        [HttpGet]
+        public string Get()
+        {
+            myLogger.LogInformation("The Get method is called in PageController.");
+            return "It's working! THe authorization was successful.";
+        }
+
+        private readonly ILogger<PageController> myLogger;
+    }
+}
---
+++ JwtTokenValidationExample/Controllers/TokenController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Text;
+
+namespace JwtTokenValidationExample.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TokenController : Controller
+    {
+        public TokenController(IConfiguration config)
+        {
+            myConfig = config;
+        }
+
+        [AllowAnonymous]
+        [HttpPost]
+        public IActionResult GetToken()
+        {
+            var tokenString = GenerateJSONWebToken();
+            var response = Ok(new { token = tokenString });
+            return response;
+        }
+
+        private string GenerateJSONWebToken()
+        {
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(myConfig["Jwt:Key"]));
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(
+                myConfig["Jwt:Issuer"],
+                myConfig["Jwt:Audience"],
+                null,
+                expires: DateTime.Now.AddMinutes(120),
+                signingCredentials: credentials);
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+
+        private readonly IConfiguration myConfig;
+    }
+}
